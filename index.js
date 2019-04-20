348169e4981a011b7129cd46e18e60db599c5da2
Matthew Listor <mattlistor@gmail.com>
2019-04-20 19:05:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function takeANumber(lineArray, newPerson){
   lineArray.push(newPerson);
   var message = "Welcome, ${newPerson}. You are number ${lineArray.length} in line";
+  console.log(message);
   return message;
 }
