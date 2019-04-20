43d35a1e836f556e414dfaf45b52b0251956321c
Matthew Listor <mattlistor@gmail.com>
2019-04-20 19:18:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(lineArray, newPerson){
   lineArray.push(newPerson);
-  var message = `Welcome, ${newPerson}. You are number ${lineArray.length} in line.` 
+  var message = `Welcome, ${newPerson}. You are number ${lineArray.length} in line.` //You have to use `grave accents` NOT 'single quotes'
   console.log(message);
   return message;
 }
