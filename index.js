a1b06e232fbfd8dbc63b85b1d63cacf831f3da60
Matthew Listor <mattlistor@gmail.com>
2019-04-20 19:11:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function takeANumber(lineArray, newPerson){
   lineArray.push(newPerson);
-  var message = "Welcome, " + newPerson + ". You are number " + lineArray.length + " in line."
+  var message = `Welcome, ${name}. You are number ${katzDeli.length} in line.`
   console.log(message);
   return message;
 }
+
+"Welcome, " + newPerson + ". You are number " + lineArray.length + " in line."
